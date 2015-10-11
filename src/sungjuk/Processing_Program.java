33d99f658f219b04601f934c920aa899af7a3233
USER <USER@192.168.123.104>
2015-10-11 21:48:08 +0900

    writeDate 구현

--- src/sungjuk/Processing_Program.java
+++ src/sungjuk/Processing_Program.java
@@ -1,6 +1,11 @@
 package sungjuk;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -59,6 +64,45 @@ static List<Student> studentList = new ArrayList<Student>();
 			}
 		}
 
+	}
+	
+	private static void readData() {
+		FileInputStream fis = null;
+		ObjectInputStream ois = null;
+		try {
+			fis = new FileInputStream(fileName);
+			ois = new ObjectInputStream(fis);
+			studentList = (ArrayList<Student>)ois.readObject();
+			System.out.println("데이터를 로드했습니다.");
+		} catch (FileNotFoundException e) {
+			System.out.println("파일이 존재하지 않습니다.");
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		} catch (ClassNotFoundException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			if(ois != null) 
+				try {ois.close();}catch(Exception e) {}
+		}		
+	}
+	
+	private static void writeData() {
+		FileOutputStream fos = null;
+		ObjectOutputStream oos = null;
+		try {
+			fos = new FileOutputStream(fileName);
+			oos = new ObjectOutputStream(fos);
+			oos.writeObject(studentList);
+			System.out.println("데이터가 저장되었습니다.");
+		} catch (FileNotFoundException e) {
+			System.out.println("파일 명이 잘 못 되었습니다.");
+		} catch (IOException e) {
+			System.out.println(e.getMessage());			
+		} finally {
+			if(oos != null) 
+				try {oos.close();}catch(Exception e) {}
+		}
+		
 	}
 
 
